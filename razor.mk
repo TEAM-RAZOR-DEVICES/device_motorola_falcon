c86826fbf663526e7c2b4bed4f955ac0a836ae66
srisurya95 <srisurya95@gmail.com>
2015-10-02 12:31:17 +0000

    razor-ify

--- razor.mk
+++ razor.mk
@@ -1,10 +1,10 @@
 # Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/razor/config/common_full_phone.mk)
 
 # Inherit device specific
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := slim_falcon
+PRODUCT_NAME := razor_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
